8e29bae82a10df79cdd524dbedecf549be5adf71
Abhi Ramani <abhiramani3@gmail.com>
2017-07-30 18:45:41 +0530

    Add Tow Forms.

---
+++ index.js
@@ -0,0 +1,106 @@
+console.log();
+
+function submit() {
+console.log("Hellow");
+}
+var x = document.getElementById('formOne');
+var y = document.getElementById('formTwo');
+
+y.style.display = 'none';
+
+
+var nname = document.getElementById('name');
+var number = document.getElementById("number");
+var doc = document.getElementById("DOC");
+var tocf = document.getElementById("tocf");
+var toct = document.getElementById("toct");
+var dor = document.getElementById("dor");
+var emailid = document.getElementById("emailid");
+var mt = document.getElementById("mt");
+var dob = document.getElementById("dob");
+var country = document.getElementById("country");
+var state = document.getElementById("state");
+var city = document.getElementById("city");
+var areaid = document.getElementById("areaid");
+var address = document.getElementById("address");
+var wors = document.getElementById("wors");
+var qualification = document.getElementById("qualification");
+var maritalS = document.getElementById("maritalS");
+var gender = document.getElementById("gender");
+var corC = document.getElementById("corC");
+var pdate = document.getElementById("pdate");
+var passNo = document.getElementById("passNo");
+var amountPaid = document.getElementById("amountPaid");
+var amountBalance = document.getElementById("amountBalance");
+var Source = document.getElementById("Source");
+
+
+
+function dooo() {
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+        y.style.display = 'none';
+    } else {
+        x.style.display = 'none';
+        y.style.display = 'block';
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+console.log(mt.value);
+if (nname.value == "") {
+  console.log("working");
+}else if (number.value == "") {
+
+}else if (doc.value == "") {
+
+}else if (tocf.value == "") {
+
+}else if (toct.value == "") {
+
+}else if (dor.value == "") {
+
+}else if (emailid.value == "") {
+
+}else if (mt.value == "") {
+
+}else if (dob.value == "") {
+
+}else if (country.value == "") {
+
+}else if (state.value == "") {
+
+}else if (city.value == "") {
+
+}else if (areaid.value == "") {
+
+}else if (address.value == "") {
+
+}else if (wors.value == "") {
+
+}else if (qualification.value == "") {
+
+}else if (maritalS.value == "") {
+
+}else if (gender.value == "") {
+
+}else if (corC.value == "") {
+
+}else if (pdate.value == "") {
+
+}else if (amountPaid.value == "") {
+
+}else if (amountBalance.value == "") {
+
+}else if (Source.value == "") {
+
+}
